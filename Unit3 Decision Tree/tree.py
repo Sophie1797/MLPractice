99f76e2fc61f2b48f7f3aa683cbd272fa393af7f
Sophie1797 <35486279+Sophie1797@users.noreply.github.com>
2018-12-02 23:31:33 +0800

    Add unit 3 code
    Add unit 3 code

--- Unit3 Decision Tree/tree.py
+++ Unit3 Decision Tree/tree.py
@@ -1,3 +1,95 @@
 from math import log #compute logxN
+import operator
 
-def calcShannonEnt(dataSet)
+def calcShannonEnt(dataSet):
+    numEntries = len(dataSet)
+    labelCounts = {}
+    for fearVec in dataSet:
+        currentLabel = fearVec[-1]
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] += 1
+    shannonEnt= 0.0000000
+    for key in labelCounts:
+        prob = float(labelCounts[key])/numEntries
+        shannonEnt -= prob * float(log(prob,2))
+    return shannonEnt
+
+def createDataSet():
+    dataSet = [[1, 1, 'yes'],
+               [1, 1, 'yes'],
+               [1, 0, 'no'],
+               [0, 1, 'no'],
+               [0, 1, 'no']]
+    labels = ['no surfaceing', 'flippers']
+    return dataSet, labels
+
+def splitDataSet(dataSet, axis, value):
+    retDataSet = []
+    for featVec in dataSet:
+        if featVec[axis] == value:
+            reducedFeatVec = featVec[:axis]
+            reducedFeatVec.extend(featVec[axis+1:])
+            retDataSet.append(reducedFeatVec)
+    return retDataSet
+
+def chooseBestFeatureToSplit(dataSet):
+    numFeatures = len(dataSet[0]) - 1
+    baseEntrpy = calcShannonEnt(dataSet)
+    bestInfoGain = 0.0
+    bestFeature = -1
+    for i in range(numFeatures):
+        featList = [example[i] for example in dataSet]
+        uniqueVals = set(featList)
+        newEntropy = 0.0
+        for value in uniqueVals:
+            subDataSet = splitDataSet(dataSet, i, value)
+            prob = len(subDataSet)/float(len(dataSet))
+            newEntropy += prob * calcShannonEnt(subDataSet)
+        infoGain = baseEntrpy - newEntropy
+        if(infoGain>bestInfoGain):
+            bestInfoGain = infoGain
+            bestFeature = i
+    return bestFeature
+
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount.keys:
+            classCount[vote]=0
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCount.iteritems(),key=operator.itemgetter(1),reverse=True)
+    return sortedClassCount[0][0]
+
+def createTree(dataSet,labels):
+    classList = [example[-1] for example in dataSet]
+    if classList.count(classList[0]) == len(classList):#类别完全相同就停止划分
+        return classList[0]
+    if len(dataSet[0]) == 1:
+        return majorityCnt(classList)#递归结束条件，如果没有特征了就返回label值最高频的那个
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel = labels[bestFeat]
+    myTree = {bestFeatLabel:{}}
+    del(labels[bestFeat])
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        splitedDataSet = splitDataSet(dataSet,bestFeat,value)
+        myTree[bestFeatLabel][value] = createTree(splitedDataSet,labels)
+    return myTree
+
+def classify(inputTree, featLabels, testVec):
+    firstStr = list(inputTree.keys())[0]
+    secondDict = inputTree[firstStr]
+    featIndex = featLabels.index(firstStr)
+    for key in list(secondDict.keys()):
+        if testVec[featIndex] == key:
+            if type(secondDict[key]).__name__=='dict':
+                classLabel = classify(secondDict[key],featLabels,testVec)
+            else:
+                classLabel = secondDict[key]
+    return classLabel
+
+myDat,labels = createDataSet()
+myTree = createTree(myDat,labels[:])#构建好以后序列化一下，以后直接deserialize就能用了
+c = classify(myTree,labels,[1,0])
